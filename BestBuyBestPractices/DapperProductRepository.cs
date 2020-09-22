71cb8666bd49fc64a678f43bbd815f8114c8bfed
aburbank <ashlynburbank@gmail.com>
2020-09-22 13:22:27 -0500

    final - DONE

--- BestBuyBestPractices/DapperProductRepository.cs
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using Dapper;
 
 namespace BestBuyBestPractices
 {
     public class DapperProductRepository : IProductRepository
     {
-        public DapperProductRepository()
+        private readonly IDbConnection _connection;
+
+        public DapperProductRepository(IDbConnection connection)
         {
+            _connection = connection;
         }
 
         public void CreateProduct(string name, double price, int categoryID)
@@ -16,7 +21,15 @@ namespace BestBuyBestPractices
 
         public IEnumerable<Product> GetAllProducts()
         {
-            throw new NotImplementedException();
+            return _connection.Query<Product>("SELECT *  FROM products;");
         }
+
+        public void InsertProduct(string newProductName, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO PRODUCTS (Name, Price, CategoryID) VALUES (@productName, @price, @categoryID);",
+            new { productName = newProductName, price = price, categoryID = categoryID });
+        }
+
+
     }
 }
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -23,13 +23,15 @@ namespace BestBuyBestPractices
 
             //-------
 
-            var repo = new DapperDepartmentRepository(conn);
+            var departmentRepo = new DapperDepartmentRepository(conn);
 
             Console.WriteLine("Type a new Department name");
 
             var newDepartment = Console.ReadLine();
 
-            var departments = repo.GetAllDepartments();
+            departmentRepo.InsertDepartment(newDepartment);
+
+            var departments = departmentRepo.GetAllDepartments();
 
             foreach (var dept in departments)
             {
@@ -39,11 +41,21 @@ namespace BestBuyBestPractices
             //-------
 
 
-            var productRepo = new DapperProductRepository();
+            var productRepo = new DapperProductRepository(conn);
 
             Console.WriteLine("Type a new Product name");
 
             var newProduct = Console.ReadLine();
+
+            Console.WriteLine("Type the Price");
+
+            var newPrice = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Type the Category ID");
+
+            var newCategoryId = int.Parse(Console.ReadLine());
+
+            productRepo.InsertProduct(newProduct, newPrice, newCategoryId);
 
             var products = productRepo.GetAllProducts();
 
